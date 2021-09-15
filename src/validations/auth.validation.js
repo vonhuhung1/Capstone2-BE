d627d840350a968b3542395054513d6f3e5d527f
tuan.vuthanh <vu.van.duy239@gmail.com>
2021-09-15 20:21:51 +0700

    fix init project

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -1,11 +1,18 @@
 const Joi = require('joi');
-const { password } = require('./custom.validation');
+const { password, objectId } = require('./custom.validation');
 
 const register = {
   body: Joi.object().keys({
+    fistName: Joi.string().min(0).max(25).required(),
+    lastName: Joi.string().min(0).max(25).required(),
     email: Joi.string().required().email(),
-    password: Joi.string().required().custom(password),
-    name: Joi.string().required(),
+    password: Joi.string().min(0).max(150).required().custom(password),
+    typeId: Joi.string().custom(objectId),
+    location: Joi.string().min(0).max(150),
+    phoneNumber: Joi.string().min(0).max(15).required(),
+    frontCard: Joi.string().min(0).max(150).required(),
+    backCard: Joi.string().min(0).max(150).required(),
+    agree: Joi.boolean().required(),
   }),
 };
 
