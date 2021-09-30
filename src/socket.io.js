95975d468186934b86494a22e4e1f32f7ebbb564
tuan.vuthanh <vu.van.duy239@gmail.com>
2021-09-30 22:26:31 +0700

    fix socket and folder

--- src/socket.io.js
+++ src/socket.io.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const https = require('http');
 const socketIO = require('socket.io');
+const cors = require('cors');
+const path = require('path');
 const logger = require('./config/logger');
 
 const app = express();
@@ -8,17 +10,29 @@ const serverSocket = https.createServer(app);
 
 const io = socketIO(serverSocket);
 
-(() => {
-  io.on('connection', (socket) => {
-    socket.on('disconnect', () => {});
-    // server listening to client
-    socket.on('Client-sent-data', (data) => {
-      // server send data to client
-      socket.emit('Server-sent-data', data);
-    });
-  });
+app.use(cors());
 
-  serverSocket.listen(8081, () => {
-    logger.info(`Socket Listening to port 8081`);
+app.options('*', cors());
+
+// use template send email
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/template/views'));
+
+app.use('/socket', (req, res) => {
+  res.render('index');
+});
+
+io.on('connection', (socket) => {
+  socket.on('disconnect', () => {});
+  // server listening to client
+  socket.on('chat-message', (data) => {
+    // server send data to client
+    io.emit('send-message', data);
   });
-})();
+});
+
+serverSocket.listen(8081, () => {
+  logger.info(`Socket Listening to port 8081`);
+});
+
+(() => {})();
