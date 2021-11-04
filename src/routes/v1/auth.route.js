1f71aacf4a64ca91367c4e1443b34017b0b0d5c5
duy.vuvan <vu.van.duy239@gmail.com>
2021-11-04 16:00:57 +0700

    fix test heroku

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -18,9 +18,9 @@ router.post(
   authController.register
 );
 router.post('/login', validate(authValidation.login), authController.login);
-router.post('/login-google', validate(authValidation.login), authController.loginGoogle);
-router.post('/login-meta', validate(authValidation.login), authController.loginMeta);
-router.post('/logout', validate(authValidation.logout), authController.logout);
+// router.post('/login-google', validate(authValidation.login), authController.loginGoogle);
+// router.post('/login-meta', validate(authValidation.login), authController.loginMeta);
+// router.post('/logout', validate(authValidation.logout), authController.logout);
 router.post('/refresh-tokens', validate(authValidation.refreshTokens), authController.refreshTokens);
 router.post('/forgot-password', validate(authValidation.forgotPassword), authController.forgotPassword);
 router.post('/reset-password', validate(authValidation.resetPassword), authController.resetPassword);
